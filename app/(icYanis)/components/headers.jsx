01139a00c12d7639b20ca9e729ccf94c5498191c
athena <legion.athenienne@gmail.com>
2024-10-10 14:14:01 +0200

    STEP1: EXTRAIRE LALOGIC DE STYLE EN SCSS (ou encore mieux,en BEM(pas fait ici))

---
+++ app/(icYanis)/components/headers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeaderDefault() {
+    return <header>
+        <h1>QUEEN</h1>
+        <p>Il faut: </p>
+        <ul>
+            <li>Proposer une liste de type de site (vitrine, ecommerce, landingpage, forum, wiki, doc, etc...)</li>
+            <li>Puis proposer une liste de catégorie sociaux-professionnelles, afin que le style et le comportement de l'application soit aligné sur cette catégorie (sociaux-professionnelles)de client</li>
+        </ul>
+    </header>
+}
--- app/(icYanis)/layout.tsx
+++ app/(icYanis)/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
+import HeaderDefault from "./components/headers"
 
+import "./assets/scss/index.scss";
 import "../globals.css";
 
 const geistSans = localFont({
@@ -30,14 +32,16 @@ export default function RootLayout({
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-      <header className="container mx-auto px-4 py-8">
+      {/* <header className="container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold mb-8 text-center">QUEEN</h1>
         <p className="text-xl text-center mb-12">Il faut: </p>
         <ul>
           <li>Proposer une liste de type de site (vitrine, ecommerce, landingpage, forum, wiki, doc, etc...)</li>
           <li>Puis proposer une liste de catégorie sociaux-professionnelles, afin que le style et le comportement de l'application soit aligné sur cette catégorie (sociaux-professionnelles)de client</li>
         </ul>
-      </header>
+      </header> */}
+      <HeaderDefault />
+
 
       {children}
         
